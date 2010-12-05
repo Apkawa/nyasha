9b9aec4d2296a517ab5f5c2ae12e64fe2fb0e206
Apkawa <apkawa@gmail.com>
2010-12-05 22:42:37 +0300

    microminor

--- nyasha/nginx-fcgi.sh
+++ nyasha/nginx-fcgi.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
     "start")
-        ./manage.py runfcgi method=prefork host=127.0.0.1 port=8666 pidfile=/tmp/server.pid --settings="settings_prod"
+        ./manage.py runfcgi method=prefork host=127.0.0.1 port=8666 pidfile=/tmp/server.pid maxchildren=7 --settings="settings_prod" 
         ;;
     "stop")
         kill -9 `cat /tmp/server.pid`
