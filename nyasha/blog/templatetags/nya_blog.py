0d97f901253b9b46bdd2c8782b42bdf050137abd
Apkawa <apkawa@gmail.com>
2010-12-12 01:54:34 +0300

    add random message and post sidebar

--- nyasha/blog/templatetags/nya_blog.py
+++ nyasha/blog/templatetags/nya_blog.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import random
 from django.conf import settings
 from django.template.defaultfilters import stringfilter
 from django import template
 
 from utils.markup import nya_parser 
 from django.contrib.auth.models import User
-from blog.models import Profile, Tag
+from blog.models import Profile, Tag, Post, Comment
 
 register = template.Library()
 
@@ -26,6 +27,16 @@ def user_top_sidebar():
     users = Profile.attach_user_info(users).order_by('?')[:5]
     context = {}
     context['users'] = users
+    context['MEDIA_URL'] = settings.MEDIA_URL
+    return context
+
+@register.inclusion_tag('blog/_post_comment_sidebar.html')
+def post_comment_sidebar():
+    posts = Post.objects.filter().select_related('user','user__profile').order_by('?')[:3]
+    comments = Comment.objects.filter().select_related('user','user__profile').order_by('?')[:3]
+    messages = sum(zip(posts, comments), ())
+    context = {}
+    context['messages'] = messages
     context['MEDIA_URL'] = settings.MEDIA_URL
     return context
 
